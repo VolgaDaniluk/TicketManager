70f461b72d0330e04fecee9ffb3299f29a724de2
Abzal <abzal0707@gmail.com>
2024-08-01 02:18:39 +0600

    Add an ability to read tickets from a file

--- src/main/java/com/danliuk/Main.java
+++ src/main/java/com/danliuk/Main.java
@@ -4,26 +4,25 @@ import com.danliuk.model.BusTicket;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws JsonProcessingException {
+        try (var reader = new BufferedReader(new FileReader("src/main/resources/ticketData.txt"))) {
+            int x = 0;
+            while (x < 5) {
+                BusTicket busTicket = new ObjectMapper().readValue(reader.readLine(), BusTicket.class);
 
-        int x = 0;
+                // TODO: ticket validation
 
-        do {
-            String input = getInput();
-            BusTicket busTicket = new ObjectMapper().readValue(input, BusTicket.class);
-
-            // TODO: ticket validation
-
-            System.out.println(busTicket.toString());
-            x++;
-
-        } while (x < 5);
-    }
-
-    private static String getInput() {
-        return new Scanner(System.in).nextLine();
+                System.out.println(busTicket.toString());
+                x++;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
