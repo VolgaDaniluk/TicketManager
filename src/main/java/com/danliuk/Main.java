32cb9765b91c76fbfc9e3b2a1f297fc0f16baf1c
Abzal <abzal0707@gmail.com>
2024-08-02 00:43:53 +0600

    Add validations and counters

--- src/main/java/com/danliuk/Main.java
+++ src/main/java/com/danliuk/Main.java
@@ -4,25 +4,50 @@ import com.danliuk.model.BusTicket;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Scanner;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) throws JsonProcessingException {
         try (var reader = new BufferedReader(new FileReader("src/main/resources/ticketData.txt"))) {
             int x = 0;
-            while (x < 5) {
-                BusTicket busTicket = new ObjectMapper().readValue(reader.readLine(), BusTicket.class);
+            ObjectMapper mapper = new ObjectMapper();
+            mapper.findAndRegisterModules();
+            int totalTickets = 5;
+            int validTickets = 0;
+            while (x < totalTickets) {
+                BusTicket busTicket = mapper.readValue(reader.readLine(), BusTicket.class);
 
                 // TODO: ticket validation
+                ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+                Validator validator = factory.getValidator();
+                Set<ConstraintViolation<BusTicket>> violations = validator.validate(busTicket);
+
+                if (!violations.isEmpty()) {
+                    for (ConstraintViolation<BusTicket> violation : violations) {
+                        System.out.println("Warning: " + violation.getMessage() + " in ticket data: " + busTicket);
+                        BusTicket.customSetViolationCounts(BusTicket.getViolationCounts(), String.valueOf(violation.getPropertyPath()));
+                        System.out.println(BusTicket.getViolationCounts());
+                    }
+                } else {
+                    validTickets++;
+                }
 
                 System.out.println(busTicket.toString());
                 x++;
             }
+
+            System.out.printf("Total = {%d}\n", totalTickets);
+            System.out.printf("Valid = {%d}\n", validTickets);
+            System.out.printf("Most popular violation = {%s}\n", BusTicket.getPropertyWithMostViolations());
         } catch (IOException e) {
-            e.printStackTrace();
+            System.out.println(e.getMessage());
         }
     }
 }
--- src/main/java/com/danliuk/model/BusTicket.java
+++ src/main/java/com/danliuk/model/BusTicket.java
@@ -1,8 +1,17 @@
 package com.danliuk.model;
 
+import javax.validation.constraints.*;
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 @Getter
 @Setter
@@ -11,9 +20,28 @@ public class BusTicket {
 
     private String ticketClass;
 
+    @NotNull(message = "Ticket type cannot be null")
+    @Pattern(regexp = "DAY|WEEK|MONTH|YEAR", message = "Ticket type must be one of DAY, WEEK, MONTH, YEAR")
     private String ticketType;
 
-    private String startDate;
+    @NotNull(message = "Start date cannot be null")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    @PastOrPresent(message = "Start date cannot be in the future")
+    private LocalDate startDate;
 
-    private String price;
+    @NotNull(message = "Price cannot be null")
+    @Min(value = 1, message = "Price must be greater than zero")
+    private BigDecimal price;
+
+    @Getter
+    private static Map<String, Integer> violationCounts = new HashMap<>();
+
+    public static void customSetViolationCounts(Map<String, Integer> violationCounts, String property) {
+        violationCounts.merge(property, 1, Integer::sum);
+        BusTicket.violationCounts = violationCounts;
+    }
+
+    public static String getPropertyWithMostViolations() {
+        return Collections.max(violationCounts.entrySet(), Map.Entry.comparingByValue()).getKey();
+    }
 }
